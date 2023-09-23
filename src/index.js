85bc9c4c7c6c5edc08a166830ffd6efe7e5c60eb
Zidan Abraham <zidangit07@gmail.com>
2023-09-23 18:10:57 +0800

    feat(components): experiment with nesting components

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Using JSX
 function Greeting() {
   return (
-    <>
-      <div>
-        <h1>My First Component</h1>
-      </div>
-      <h2>Hello World</h2>
-      <input type="text" />
-    </>
+    <div>
+      <Person />
+      <Message />
+    </div>
   );
 }
 
-// Not using JSX
-// function Greeting() {
-//   return React.createElement('div', {}, React.createElement('h1', {}, 'My First Component'));
-// }
+const Person = () => <h2>Zidan Abraham</h2>;
+const Message = () => {
+  return <p>Currently learning React JS</p>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
