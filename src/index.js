65ce52ea42bcd605e0d7c310bea81732d678c505
Zidan Abraham <zidangit07@gmail.com>
2023-09-23 15:53:13 +0800

    chore(jsx): experiment with JSX syntax and rules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Using JSX
 function Greeting() {
-  return <h1>My First Component</h1>;
+  return (
+    <>
+      <div>
+        <h1>My First Component</h1>
+      </div>
+      <h2>Hello World</h2>
+      <input type="text" />
+    </>
+  );
 }
+
+// Not using JSX
+// function Greeting() {
+//   return React.createElement('div', {}, React.createElement('h1', {}, 'My First Component'));
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
