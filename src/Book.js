c7fd1e00ae5807b44aa9c46d34a263ae4ec6a92f
Zidan Abraham <zidangit07@gmail.com>
2023-09-25 17:32:24 +0800

    feat(components): add 'number' to Book component

--- src/Book.js
+++ src/Book.js
@@ -1,6 +1,9 @@
-const Book = ({ title, author, img }) => {
+const Book = ({ title, author, img, number }) => {
   return (
-    <article className="flex w-full flex-col items-center justify-between gap-3 rounded-2xl border-[1px] border-slate-300 bg-gradient-to-br from-slate-800 to-slate-600 p-4 shadow-md">
+    <article className="relative flex w-full flex-col items-center justify-between gap-3 overflow-hidden rounded-2xl border-[1px] border-slate-300 bg-gradient-to-br from-slate-800 to-slate-600 p-4 shadow-md">
+      <span className="absolute left-0 top-0 rounded-br-2xl bg-gradient-to-br from-purple-500 to-purple-900 px-2 py-3 font-medium">
+        #{number + 1}
+      </span>
       <h2 className="text-center text-lg font-medium">{title}</h2>
       <img className="w-4/5 object-cover " src={img} alt={title} />
       <h4 className="text-lg ">{author}</h4>
--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ const BookList = () => {
   return (
     <main className="flex flex-col gap-5 px-7 py-5 text-white">
       <h1 className="bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text text-center text-4xl font-bold text-transparent">
-        Best Seller Books
+        Best Sellers Books
       </h1>
       <SearchBook />
       <section className="grid w-full grid-cols-1 justify-items-center gap-5 tablet:grid-cols-3 laptop:grid-cols-4">
-        {books.map((book) => (
-          <Book key={book.id} {...book} />
+        {books.map((book, index) => (
+          <Book key={book.id} {...book} number={index} />
         ))}
       </section>
     </main>
