b16f995f685c5d1a23d96fb5f189cc0f138ae581
Zidan Abraham <zidangit07@gmail.com>
2023-09-25 17:18:21 +0800

    refactor(asset): move image assets from public to src

--- src/books.js
+++ src/books.js
@@ -1,27 +1,32 @@
+import book1 from "./assets/images/book-1.jpg";
+import book2 from "./assets/images/book-2.jpg";
+import book3 from "./assets/images/book-3.jpg";
+import book4 from "./assets/images/book-4.jpg";
+
 const books = [
   {
     id: 1,
     title: "The Shadow Work Journal",
     author: "Keila Shaheen",
-    img: "./assets/images/book-1.jpg",
+    img: book1,
   },
   {
     id: 2,
     title: "Atomic Habits",
     author: "James Clear",
-    img: "./assets/images/book-2.jpg",
+    img: book2,
   },
   {
     id: 3,
     title: "Build the Life You Want",
     author: "Arthur C. Brooks, Oprah Winfrey",
-    img: "./assets/images/book-3.jpg",
+    img: book3,
   },
   {
     id: 4,
     title: "The 48 Laws of Power",
     author: "Robert Greene",
-    img: "./assets/images/book-4.jpg",
+    img: book4,
   },
 ];
 
